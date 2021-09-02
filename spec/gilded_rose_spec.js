b787bd71fa247fc43cd2a455a6e464ec9996841b
Else Meijerink <p.e.meijerink@gmail.com>
2021-09-02 18:13:20 +0100

    Normal item sellIn reduces by 1 when quality is updated

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -6,9 +6,20 @@ describe("Gilded Rose", function () {
     shop = new Shop();
   });
 
-  it("returns item name", function () {
+  it("returns normal item name", function () {
     shop.items = [new Item("foo", 0, 0)];
     const items = shop.updateQuality();
     expect(items[0].name).toEqual("foo");
+  });
+
+  it("lowers normal item sellIn by 1", function () {
+    shop.items = [new Item("foo", 10, 0)];
+    const items = shop.updateQuality();
+    expect(items[0].sellIn).toEqual(9);
+  });
+
+  it("does not lower sellIn of normal item if it is 0", function () {
+    shop.items = [new Item("foo", 0, 0)];
+    expect(shop.items[0].sellIn).toEqual(0);
   });
 });
